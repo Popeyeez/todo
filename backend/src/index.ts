53513e36986e8b530a312412c1c3e1741ea6803d
25LP9846 25LP9846 <25LP9846@G301-CC0189.local>
2025-10-01 12:05:07 +0800

    delete , update hiiv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { nanoid } from "nanoid";
 const app: Application = express();
 const port = 3000;
 
-const tasks = [
+let tasks = [
   { id: nanoid(), name: "MacBook" },
   { id: nanoid(), name: "iMac" },
 ];
@@ -19,22 +19,32 @@ app.get("/tasks", (req: Request, res: Response) => {
 });
 
 app.post("/tasks", (req: Request, res: Response) => {
-  //app product to database
-
   const id = nanoid();
   const { name } = req.body;
+  if (!name) {
+    res.status(400).send({ message: "name cant be empty" });
+    return;
+  }
+
   tasks.unshift({ id, name });
   res.status(201).send({ id });
 });
 
 app.delete("/tasks/:id", (req: Request, res: Response) => {
-  const id = req.params;
-  res.send([]);
+  const id = req.params.id;
+  const newTasks = tasks.filter((task) => task.id !== id);
+  tasks = newTasks;
+  res.sendStatus(204);
 });
 
 app.put("/tasks/:id", (req: Request, res: Response) => {
-  const id = req.params;
-  res.send([]);
+  const id = req.params.id;
+  const { name } = req.body;
+
+  const index = tasks.findIndex((task) => task.id === id);
+  tasks[index].name = name;
+
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,16 +6,19 @@ export default function Home() {
   const [tasks, setTasks] = useState<{ id: string; name: string }[]>([]);
 
   async function createNewTask() {
-    await fetch("http://localhost:3000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: newTask }),
-    });
-    loadTasks();
-    setNewTask("");
+    if (newTask) {
+      await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newTask }),
+      });
+      loadTasks();
+      setNewTask("");
+    }
   }
+
   function loadTasks() {
     fetch("http://localhost:3000/tasks")
       .then((res) => res.json())
@@ -23,6 +26,30 @@ export default function Home() {
         setTasks(data);
       });
   }
+
+  async function deleteTask(id: string) {
+    if (confirm("Delete?")) {
+      await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: "DELETE",
+      });
+      loadTasks();
+    }
+  }
+
+  async function editTask(task: { id: string; name: string }) {
+    const newName = prompt("Edit", task.name);
+    if (newName) {
+      await fetch(`http://localhost:3000/tasks/${task.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newName }),
+      });
+      loadTasks();
+    }
+  }
+
   useEffect(() => {
     loadTasks();
   }, []);
@@ -34,13 +61,35 @@ export default function Home() {
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
         />
-        <button className="btn btn-accent" onClick={createNewTask}>
+        <button
+          disabled={!newTask}
+          className="btn btn-accent"
+          onClick={createNewTask}
+        >
           Add
         </button>
       </div>
+
       {tasks.map((task) => (
-        <div className="card p-4 border border-base-300 mt-4" key={task.id}>
-          {task.name}
+        <div
+          className="card p-4 border border-base-300 mt-4 w-100"
+          key={task.id}
+        >
+          <div className="flex items-center gap-2">
+            <input onChange={""} type="checkbox"></input>
+
+            <div className="flex-1"> {task.name} </div>
+            <button onClick={() => editTask(task)} className="btn btn-ghost">
+              Edit
+            </button>
+
+            <button
+              onClick={() => deleteTask(task.id)}
+              className="btn btn-error btn-soft"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       ))}
     </div>
